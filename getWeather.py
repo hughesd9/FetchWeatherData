10e039be0073000869c8137646d2fdf730b1fc21
David Hughes <hughesd9@tcd.ie>
2020-07-28 15:31:39 +0100

    Load in the imports that will be used. Get location from user input in the command line. Prints the location.

--- getWeather.py
+++ getWeather.py
@@ -1,5 +1,16 @@
-#TODO 
-#Get location
+#getWeather.py prints the weather for a given location
+import json
+import requests
+import sys
+#TODO
+#Get location from the command line
+if len(sys.argv) < 2:
+    print('Usage: getWeather.py place_name, 2-letter_country_code')
+    sys.exit()
+location = ''.join(sys.argv[1:])
+
+print(location)
+
 
 #Get weather data
 
